e9a01c2c90b03157c43658b7ee4bc20fb8ff6cac
Cees-Jan Kiewiet <hi@ceesjankiewiet.nl>
2019-01-03 22:27:38 +0100

    Spacing

--- tests/WhileTest.php
+++ tests/WhileTest.php
@@ -22,6 +22,7 @@ final class WhileTest extends TestCase
 
         $loop = Factory::create();
         $recoil = ReactKernel::create($loop);
+
         $recoil->setExceptionHandler(function (Throwable $error): void {
             throw $error;
         });
@@ -32,6 +33,7 @@ final class WhileTest extends TestCase
                 $output[] = $i;
             }
         });
+
         $loop->run();
 
         self::assertSame($input, $output);
@@ -42,12 +44,15 @@ final class WhileTest extends TestCase
     {
         $loop = Factory::create();
         $subject = new Subject();
+
         $loop->futureTick(function () use ($subject): void {
             $subject->onCompleted();
         });
+
         $observableWhile = observableWhile($subject);
         $promise = $observableWhile->get();
         $null = $this->await($promise, $loop);
+
         self::assertNull($null);
         self::assertSame(0, \gc_collect_cycles());
     }
